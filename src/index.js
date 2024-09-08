b08d99c9a47c0b5aa7d75776fcfeb925a3ccb0ec
nsensical <iss.doli@gmail.com>
2024-09-08 19:47:38 -0400

    added and edited JS

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/** @format */
+function displayCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", displayCity);
