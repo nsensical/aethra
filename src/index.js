f2a2ae88b27c14ee98d88f0029d4854048a877f3
nsensical <iss.doli@gmail.com>
2024-09-09 14:20:58 -0400

    Edited JS
    Added API Integration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 /** @format */
+
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "abdtf489166e9o460ad2e43465ab3d66";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
 function displayCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", displayCity);
+
+searchCity("Caracas");
