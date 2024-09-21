1c0ec21bfe1153e55f0f33244eed69d32926905c
nsensical <iss.doli@gmail.com>
2024-09-20 20:56:20 -0400

    Injected HTML into JS

--- src/index.js
+++ src/index.js
@@ -56,4 +56,30 @@ function displayCity(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", displayCity);
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+              <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forecast-icon">⛅</div>
+                <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature">
+                  <strong>15<span class="symbol">°</span></strong>
+                </div>
+                <div class="weather-forecast-temperature">
+                  9<span class="symbol">°</span>
+                </div>
+               </div>
+              </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 searchCity("Caracas");
+displayForecast();
