5f0f974f3f8fea6ca70b6702c5893635e3838494
nsensical <iss.doli@gmail.com>
2024-09-21 23:50:49 -0400

    Finished forecast
    API Integration

--- src/index.js
+++ src/index.js
@@ -58,6 +58,13 @@ function displayCity(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", displayCity);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "abdtf489166e9o460ad2e43465ab3d66";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -65,25 +72,31 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-              <div class="weather-forecast-date">${day}</div>
-              <div class="weather-forecast-icon">⛅</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+              <div class="weather-forecast-date">${formatDay(day.time)}</div>
+              <img src="${
+                day.condition.icon_url
+              }" class="weather-forecast-icon" />
                 <div class="weather-forecast-temperatures">
                 <div class="weather-forecast-temperature">
-                  <strong>15<span class="symbol">°</span></strong>
+                  <strong>${Math.round(
+                    day.temperature.maximum
+                  )}<span class="symbol">°</span></strong>
                 </div>
                 <div class="weather-forecast-temperature">
-                  9<span class="symbol">°</span>
+                  ${Math.round(
+                    day.temperature.minimum
+                  )}<span class="symbol">°</span>
                 </div>
                </div>
               </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
